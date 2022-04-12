4a4c3ad0ac65bfa59cb11d3323534ebddb0b4bcf
jsousa02 <jorgedaniel.sousa02@gmail.com>
2022-04-12 19:01:01 +0100

    add sorting option to scenario 1

--- includes/scenario1.hpp
+++ includes/scenario1.hpp
@@ -2,14 +2,22 @@
 #define DA_PROJ1_SCENARIO1_HPP
 
 #include "dataset.hpp"
-
 #include "van.hpp"
 #include "order.hpp"
 
+#include <iostream>
+#include <string>
+
 /**
- * 
  * @brief the number of vans is minimized and the number of orders delivered is maximized
+ *
+ * @param option how to sort the vans and orders
+ *
+ * @details option must be "volume" or "weight"
+ *
+ * @note the vans are sorted by <option> in descending order
+ * @note the orders are sorted by <option> in ascending order
  */
-Dataset scenario1();
+Dataset scenario1(std::string &option);
 
 #endif //DA_PROJ1_SCENARIO1_HPP
--- src/scenario1.cpp
+++ src/scenario1.cpp
@@ -3,25 +3,43 @@
 #include <vector>
 #include <algorithm>
 
-Dataset scenario1() {
+Dataset scenario1(std::string &option) {
     Dataset dataset = Dataset::load();
     std::vector<Order> orders = dataset.getOrders();
     std::vector<Van> vans = dataset.getVans();
 
-    std::sort(vans.begin(), vans.end(), [] (const Van &v1, const Van &v2) {
-        if(v1.getMaxVolume() == v2.getMaxWeight()) {
+    if(option == "volume") {
+        std::sort(vans.begin(), vans.end(), [] (const Van &v1, const Van &v2) {
+            if(v1.getMaxWeight() == v2.getMaxWeight()) {
+                return v1.getMaxWeight() > v2.getMaxWeight();
+            }
+            return v1.getMaxVolume() > v2.getMaxVolume();
+        });
+
+        std::sort(orders.begin(), orders.end(), [] (const Order &order1, const Order &order2) {
+            if(order1.getVolume() == order2.getVolume()) {
+                return order1.getWeight() < order2.getWeight();
+            }
+            return order1.getVolume() < order2.getVolume();
+        });
+    } else if (option == "weight") {
+        std::sort(vans.begin(), vans.end(), [] (const Van &v1, const Van &v2) {
+            if(v1.getMaxVolume() == v2.getMaxVolume()) {
+                return v1.getMaxVolume() > v2.getMaxVolume();
+            }
             return v1.getMaxWeight() > v2.getMaxWeight();
-        }
-        return v1.getMaxVolume() > v2.getMaxVolume();
-    });
+        });
 
-    std::sort(orders.begin(), orders.end(), [] (const Order &order1, const Order &order2) {
-        if(order1.getVolume() == order2.getVolume()) {
+        std::sort(orders.begin(), orders.end(), [] (const Order &order1, const Order &order2) {
+            if(order1.getWeight() == order2.getWeight()) {
+                return order1.getVolume() < order2.getVolume();
+            }
             return order1.getWeight() < order2.getWeight();
-        }
-        return order1.getVolume() < order2.getVolume();
-    });
-
+        });
+    } else {
+        std::cerr << "Invalid option\n";
+    }
+    
     auto vanIterator = vans.begin();
     auto orderIterator = orders.begin();
 
